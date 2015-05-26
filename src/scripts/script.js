40960b8162ce3abc2a06fbe68f4397eca76fff7c
Andrew Williams <andrewleighwilliams@gmail.com>
2015-05-26 11:14:33 -0400

    now creating cat objects during the loop and logging the array of cat objects created during the loop.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,10 @@
+var Cat = function(name, cat_num, image, counter) {
+	this.name = name;
+	this.cat_number = cat_num;
+	this.image_tag = image;
+	this.counter_tag = counter;
+};
+
 // Function that takes in the element that is clicked on and increments it's counter
 $('.cats-container').click(function(e) {
 	  	//the element has been clicked... do stuff here
@@ -17,6 +24,9 @@ var catGenerator = function() {
 					var cat_div = '<div id="cat_div' + cat_num + '"></div>';
 					var image = '<img id="kitten' + cat_num + '" src="https://placekitten.com/g/200/300" alt="A photo of a cat">';
 			    	var counter = '<h3>You have clicked on the cat <span id="x' + cat_num + '">0</span> times!</h3>';
+			    	var name = 'cat_div'+cat_num;
+			    	var cat = new Cat(name, cat_num, image, counter);
+			    	cat_objects.push(cat);
 			    	$('.cats-container').append(cat_div);
 			    	$('#cat_div' + cat_num).append(image);
 			    	$('#cat_div' + cat_num).append(counter);
@@ -25,9 +35,10 @@ var catGenerator = function() {
 			});
 		});
 	};
-	
-var Cat = {
-	name: name
-};
+
+var cat_objects = [];
+
 
 document.addEventListener("load", catGenerator());
+
+console.log(cat_objects);
